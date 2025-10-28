680b0fc86fdce029e5b7675cb85ef80a031500b1
lckjcnWq <17324432872@163.com>
2025-10-28 09:56:22 +0800

    update

--- example01.py
+++ example01.py
@@ -1,3 +1,16 @@
 print('hello world')
 print('this is a test')
 print(0x100)
+
+a=45
+b=12
+print(a+b)
+print(a*b)
+print(str(a))
+print(float(b))
+print(bool(a))
+
+flag0 =  1==1
+print(flag0)
+flag1 = 3>1
+print(flag1)
