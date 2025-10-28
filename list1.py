0d1060d34ffd02698e13c84801f82e89b70b2a9f
lckjcnWq <17324432872@163.com>
2025-10-28 17:23:46 +0800

    update

---
+++ list1.py
@@ -0,0 +1,14 @@
+t1  = (100,200,300)
+# 定义一个四元组
+t2 = ('骆昊', 45, True, '四川成都')
+
+print(type(t1))
+print(type(t2))
+
+print(len(t1))
+print(t1[0])
+
+print(t2[:2])
+
+for elem in t1:
+    print(elem)
