1435ea111a28af9ea18d30db0ca1fd2b6c3f31b4
lckjcnWq <17324432872@163.com>
2025-10-28 10:13:55 +0800

    update

--- example01.py
+++ example01.py
@@ -1,16 +1,43 @@
-print('hello world')
-print('this is a test')
-print(0x100)
+import time
+from pydoc import describe
 
-a=45
-b=12
-print(a+b)
-print(a*b)
-print(str(a))
-print(float(b))
-print(bool(a))
+# print('hello world')
+# print('this is a test')
+# print(0x100)
+#
+# a=45
+# b=12
+# print(a+b)
+# print(a*b)
+# print(str(a))
+# print(float(b))
+# print(bool(a))
+#
+# flag0 =  1==1
+# print(flag0)
+# flag1 = 3>1
+# print(flag1)
+#
+# # 判断条件
+#
+# height = float(input('please input your height: '))
+# width = float(input('please input your width: '))
+# bmi = height/ (width /100) ** 2
+# print(bmi)
+# if 18.5 <= bmi <= 25:
+#     print('你的身材很棒')
+# else:
+#     print('你的身材不够标准！')
+#
+# status_code = int(input('please input your status code: '))
+# match status_code:
+#     case 1: describe='good'
+#     case 2: describe='bad'
+#     case 3: describe='ok'
+#     case _: describe='error'
+# print(describe)
 
-flag0 =  1==1
-print(flag0)
-flag1 = 3>1
-print(flag1)
+# 循环条件
+for i in range(3600):
+    print(i)
+    time.sleep(1)
